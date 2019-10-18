0a2936dfeeed3e3a77715dc78ddfce206dab9470
rbarthe <rbarthe@wemanity.com>
2019-10-18 15:42:41 +0200

    Modification inital setup

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../../assests/images/logo.svg';
 
 import './App.css';
 
@@ -7,18 +6,9 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Birthday choice
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
