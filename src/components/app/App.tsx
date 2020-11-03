33a437ea55c382987e6a621758a345f55a0bfdb9
Juan Velasquez <jvelasquez@unac.edu.co>
2020-11-03 10:16:07 +0100

    Initial configuration

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 
 import "./App.scss";
+import Table from "../Table";
 
 const App: React.FC = () => {
-	return (
-		<div className="app">
-			<header className="app-header">
-				<p>React app</p>
-			</header>
-		</div>
-	);
+  return (
+    <div className="app">
+      <header className="app-header">
+        <Table></Table>
+      </header>
+    </div>
+  );
 };
 
 export default App;
